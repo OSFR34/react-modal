f5439b10a3ccc645371f12860f839b9a833d8fd6
OSFR34 <selma-ozturk@outlook.com>
2022-09-24 19:04:21 +0200

    suppression des problèmes vsc

--- src/Modal.js
+++ src/Modal.js
@@ -1,35 +1,41 @@
+/* eslint-disable prettier/prettier */
 import React from 'react'
+// eslint-disable-next-line prettier/prettier
 import styles from './styles.module.css'
 
-export default function Modal(
-    {
-        show = false,
-        title = "Modal Title",
-        content = 'Modal Content'
-    }
+export default function Modal({
+  show = false,
+  title = 'Modal Title',
+  content = 'Modal Content'
+}) {
+  const closeModal = () => {
+    document.querySelector('#modal').display = 'none'
+  }
 
-){
-
-    const closeModal = () => {
-        document.querySelector('#modal').display = 'none';
-    }
-
-    return (
-        <div className={show ? styles.modalshow : styles.modalhide}>
-            <div className={styles.modalbody}>
-               <div className={styles.modaltitle}>
-                    {title} 
-                </div>              
-                <div className={styles.modalcontent}>
-                    {content}
-                </div>
-                <a className={styles.close}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle"><circle cx="12" cy="12" r="10"/><line x1="15" y1="9" x2="9" y2="15"/><line x1="9" y1="9" x2="15" y2="15"/></svg>
-                </a>
-              
-
-            </div>
-        </div>   
-       
-    )
+  return (
+    <div className={show ? styles.modalshow : styles.modalhide}>
+      <div className={styles.modalbody}>
+        <div className={styles.modaltitle}>{title}</div>
+        <div className={styles.modalcontent}>{content}</div>
+        <a className={styles.close}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='1'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            className='feather feather-x-circle'
+          >
+            <circle cx='12' cy='12' r='10' />
+            <line x1='15' y1='9' x2='9' y2='15' />
+            <line x1='9' y1='9' x2='15' y2='15' />
+          </svg>
+        </a>
+      </div>
+    </div>
+  )
 }
